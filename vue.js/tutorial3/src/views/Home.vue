31c7be6255e817750fc2c708baa09fe696c824c6
codelieche <codelieche@gmail.com>
2020-12-26 20:52:23 +0800

    add vue.js tutorial3 project

---
+++ vue.js/tutorial3/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    Hello Vue3.js
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+  },
+});
+</script>
